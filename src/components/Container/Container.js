5d6f248afa23f132c0fbdba54c0d458461db3ca2
Seongmin Park <wluns32@gmail.com>
2017-08-19 14:30:58 +0900

    Add 404 Not Found page

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
-import { Home, About, Contacts } from '../';
+import { Home, About, Contacts, NotFound } from '../';
 
 class Container extends Component {
   render() {
     return (
       <div>
+        <Switch>
           <Route exact path='/' component={Home}/>
           <Route path='/about' component={About}/>
           <Route path='/contacts' component={Contacts}/>
+          <Route component={NotFound}/>
+        </Switch>
       </div>
     );
   }
--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Container from './Container/Container';
 import About from './About/About';
 import Contacts from './Contacts/Contacts';
 import Home from './Home/Home';
+import NotFound from './NotFound/NotFound';
 
 export {
     NavBar,
     Container,
     About,
     Contacts,
-    Home
+    Home,
+    NotFound
 };
